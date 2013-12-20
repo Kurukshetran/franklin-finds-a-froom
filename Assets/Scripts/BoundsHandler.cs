19c788b2441be18bb3e732a6556218b28db36aa7
jon uy <jonxuy@gmail.com>
2013-12-20 01:52:53 -0500

    Tiles bounce when hit and character objects switch sides when running offscreen

---
+++ Assets/Scripts/BoundsHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundsHandler : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		float newX = other.gameObject.transform.position.x * -1;
+		other.gameObject.transform.position = new Vector3(newX, other.gameObject.transform.position.y);
+
+	}
+}
---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTile : MonoBehaviour {
+
+	private Transform tileRenderer;
+	
+	void Awake() {
+		tileRenderer = transform.FindChild("tileRenderer");
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			// Should be sufficient only checking the initial contact point
+			bool contactFromBelow = coll.contacts[0].normal.x == 0 && coll.contacts[0].normal.y == 1;
+			if (!tileRenderer.animation.isPlaying && contactFromBelow)
+				tileRenderer.animation.Play();
+		}
+	}
+}
