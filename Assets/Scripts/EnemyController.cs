5629c2d65bfa0ffde14ca635fadd75121ee34ee2
jon uy <jonxuy@gmail.com>
2013-12-29 09:52:50 -0500

    Added animation and logic to disable enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	#region Public
 	// Movement speed.
-	public float speed = 5;
+	public float speed = 5f;
+
+	// Magnitude of the bounce when enemy is disabled
+	public float disableBounceVelocity = 10f;
+	#endregion
+
+	#region Private
+	// Handle to the Animation
+	private Animator animator;
 
 	// If true, enemy movement is to the right.
 	private bool directionToRight = true;
 
-	void Update() {
+	private enum EnemyState {
+		NORMAL,
+		DISABLED,
+		DEAD
+	};
+
+	private EnemyState currState = EnemyState.NORMAL;
+	private EnemyState nextState = EnemyState.NORMAL;
+	#endregion
+
+	void Start() {
+		Transform renderObject = transform.Find("renderObject");
+		animator = renderObject.GetComponent<Animator>();
 	}
 
 	void FixedUpdate() {
@@ -17,12 +38,24 @@ public class EnemyController : MonoBehaviour {
 		float enemySpeed = speed * dir;
 		rigidbody2D.velocity = new Vector2(transform.localScale.x * enemySpeed, rigidbody2D.velocity.y);
 
-		if (this.directionToRight) {
+		if (this.directionToRight && transform.eulerAngles.y != 180) {
 			transform.eulerAngles = new Vector3(0, 180f, 0);
+		}
+
+		if (nextState == EnemyState.DISABLED && currState != EnemyState.DISABLED) {
+			currState = EnemyState.DISABLED;
+
+			// Stop forward motion and trigger disabled animation
+			speed = 0;
+			animator.SetBool("Disabled", true);
 		}
 	}
 
 	public void SetDirection(bool dir) {
 		this.directionToRight = dir;
+	}
+
+	public void SetDisabled() {
+		nextState = EnemyState.DISABLED;
 	}
 }
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -4,17 +4,31 @@ using System.Collections;
 public class GroundTile : MonoBehaviour {
 
 	private Transform tileRenderer;
-	
+
+	private Transform enemyCheck;
+
 	void Awake() {
 		tileRenderer = transform.FindChild("tileRenderer");
+		enemyCheck = transform.FindChild("enemyCheck");
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Player") {
+			// Play the bounce animation on the tile
 			// Should be sufficient only checking the initial contact point
 			bool contactFromBelow = coll.contacts[0].normal.x == 0 && coll.contacts[0].normal.y == 1;
-			if (!tileRenderer.animation.isPlaying && contactFromBelow)
+			if (!tileRenderer.animation.isPlaying && contactFromBelow) {
 				tileRenderer.animation.Play();
+			}
+
+			// If an enemy is above the tile, flip over the enemy
+			if (enemyCheck) {
+				RaycastHit2D hit = Physics2D.Linecast(transform.position, enemyCheck.position, 1 << LayerMask.NameToLayer("Enemy"));
+				if (hit && hit.transform) {
+					EnemyController enemyController = hit.transform.GetComponent<EnemyController>();
+					enemyController.SetDisabled();
+				}
+			}
 		}
 	}
 }
