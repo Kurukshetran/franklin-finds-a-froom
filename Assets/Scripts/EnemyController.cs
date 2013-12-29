30fdb746a6b8dbd4162a5d6e72f4a6f0793aef24
jon uy <jonxuy@gmail.com>
2013-12-28 21:02:43 -0500

    Added spawn points and a test enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	// Movement speed.
+	public float speed = 5;
+
+	// If true, enemy movement is to the right.
+	private bool directionToRight = true;
+
+	void Update() {
+	}
+
+	void FixedUpdate() {
+		int dir = this.directionToRight ? 1 : -1;
+		float enemySpeed = speed * dir;
+		rigidbody2D.velocity = new Vector2(transform.localScale.x * enemySpeed, rigidbody2D.velocity.y);
+
+		if (this.directionToRight) {
+			transform.eulerAngles = new Vector3(0, 180f, 0);
+		}
+	}
+
+	public void SetDirection(bool dir) {
+		this.directionToRight = dir;
+	}
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+
+	// Array of enemy prefabs to spawn.
+	public GameObject[] enemyPrefabs;
+
+	// Time in seconds between spawns.
+	public float spawnDelay;
+
+	// Time in seconds before the first spawn.
+	public float spawnStart;
+
+	// 
+	public bool directionToRight = false;
+
+	// If true, randomize the type of enemy spawned.
+	public bool randomizeEnemies = false;
+
+	void Start() {
+		InvokeRepeating("Spawn", spawnStart, spawnDelay);
+	}
+
+	void Spawn() {
+		GameObject enemy = (GameObject)Instantiate(enemyPrefabs[0], transform.position, transform.rotation);
+		EnemyController enemyController = enemy.GetComponent<EnemyController>();
+		enemyController.SetDirection(directionToRight);
+	}
+}
