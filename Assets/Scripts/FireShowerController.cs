8e9cc1151dcc88701af440ba5e909ecefff60709
jon uy <jonxuy@gmail.com>
2014-05-06 00:25:51 -0400

    Added audio for the pre-fire shower shake
    Closes #18

--- Assets/Scripts/FireShowerController.cs
+++ Assets/Scripts/FireShowerController.cs
@@ -3,111 +3,117 @@ using System.Collections;
 
 public class FireShowerController : MonoBehaviour {
 
-	// Handle to the main camera
-	public Camera mainCamera;
+    // Handle to the main camera
+    public Camera mainCamera;
 
-	// Handle to the fireball prefab
-	public GameObject fireballObj;
+    // Handle to the fireball prefab
+    public GameObject fireballObj;
 
-	// Camera shake intensity before shower
-	public float shakeIntensity;
+    // Camera shake intensity before shower
+    public float shakeIntensity;
 
-	// Shake intensity's decay
-	public float shakeDecay;
+    // Shake intensity's decay
+    public float shakeDecay;
 
-	private FireShower[] showerConfigs;
+    // Audio to play when shake occurs.
+    public AudioClip audioShake;
 
-	private Vector3 camOriginPos;
-	private Quaternion camOriginRot;
+    private FireShower[] showerConfigs;
 
-	private float startDelay;
+    private Vector3 camOriginPos;
+    private Quaternion camOriginRot;
 
-	private float currShakeIntensity;
-	private bool pendingShower;
+    private float startDelay;
 
-	private int currentShower;
+    private float currShakeIntensity;
+    private bool pendingShower;
 
-	void Update () {
-		if (startDelay > 0) {
-			startDelay -= Time.deltaTime;
-			if (startDelay <= 0) {
-				StartShake();
-			}
-		}
+    private int currentShower;
 
-		if (pendingShower) {
-			if (currShakeIntensity > 0) {
-				mainCamera.transform.position = camOriginPos + Random.insideUnitSphere * currShakeIntensity;
-				mainCamera.transform.rotation = new Quaternion(
-					camOriginRot.x + Random.Range(-currShakeIntensity, currShakeIntensity),
-					camOriginRot.y + Random.Range(-currShakeIntensity, currShakeIntensity),
-					camOriginRot.z + Random.Range(-currShakeIntensity, currShakeIntensity),
-					camOriginRot.w + Random.Range(-currShakeIntensity, currShakeIntensity));
-				currShakeIntensity -= shakeDecay;
-			}
-			else {
-				StartShower();
-			}
-		}
-	}
+    void Update () {
+        if (startDelay > 0) {
+            startDelay -= Time.deltaTime;
+            if (startDelay <= 0) {
+                StartShake();
+            }
+        }
 
-	/**
-	 * Trigger the camera shake prior to a fire shower.
-	 */
-	private void StartShake() {
-		camOriginPos = mainCamera.transform.position;
-		camOriginRot = mainCamera.transform.rotation;
-		currShakeIntensity = shakeIntensity;
+        if (pendingShower) {
+            if (currShakeIntensity > 0) {
+                mainCamera.transform.position = camOriginPos + Random.insideUnitSphere * currShakeIntensity;
+                mainCamera.transform.rotation = new Quaternion(
+                    camOriginRot.x + Random.Range(-currShakeIntensity, currShakeIntensity),
+                    camOriginRot.y + Random.Range(-currShakeIntensity, currShakeIntensity),
+                    camOriginRot.z + Random.Range(-currShakeIntensity, currShakeIntensity),
+                    camOriginRot.w + Random.Range(-currShakeIntensity, currShakeIntensity));
+                currShakeIntensity -= shakeDecay;
+            }
+            else {
+                StartShower();
+            }
+        }
+    }
 
-		pendingShower = true;
-	}
+    /**
+     * Trigger the camera shake prior to a fire shower.
+     */
+    private void StartShake() {
+        camOriginPos = mainCamera.transform.position;
+        camOriginRot = mainCamera.transform.rotation;
+        currShakeIntensity = shakeIntensity;
 
-	/**
-	 * Spawn the fireballs and randomize their starting positions.
-	 */
-	private void StartShower() {
-		pendingShower = false;
+        pendingShower = true;
 
-		System.Random random = new System.Random();
+        // Play shake audio
+        AudioSource.PlayClipAtPoint(this.audioShake, transform.position);
+    }
 
-		FireShower config = showerConfigs[currentShower];
-		for (int i = 0; i < config.numFireballs; i++) {
-			GameObject obj = (GameObject)Instantiate(fireballObj);
+    /**
+     * Spawn the fireballs and randomize their starting positions.
+     */
+    private void StartShower() {
+        pendingShower = false;
 
-			float randX = (float)random.NextDouble();
-			float deltaX = randX * transform.localScale.x;
-			float xPos = (-1 * (transform.localScale.x / 2)) + deltaX;
+        System.Random random = new System.Random();
 
-			float randY = (float)random.NextDouble();
-			float deltaY = randY * 30;
-			float yPos = transform.position.y + deltaY;
-			obj.transform.position = new Vector3(xPos, yPos, transform.position.z);
+        FireShower config = showerConfigs[currentShower];
+        for (int i = 0; i < config.numFireballs; i++) {
+            GameObject obj = (GameObject)Instantiate(fireballObj);
 
-			obj.rigidbody2D.gravityScale = config.gravityScale;
-		}
+            float randX = (float)random.NextDouble();
+            float deltaX = randX * transform.localScale.x;
+            float xPos = (-1 * (transform.localScale.x / 2)) + deltaX;
 
-		// If there's another shower, start the timer for it
-		if (showerConfigs.Length > currentShower + 1) {
-			currentShower++;
-			startDelay = showerConfigs[currentShower].startDelay;
-		}
-	}
+            float randY = (float)random.NextDouble();
+            float deltaY = randY * 30;
+            float yPos = transform.position.y + deltaY;
+            obj.transform.position = new Vector3(xPos, yPos, transform.position.z);
 
-	/**
-	 * Setup the shower configs.
-	 */
-	public void Setup(FireShower[] showers) {
-		showerConfigs = showers;
+            obj.rigidbody2D.gravityScale = config.gravityScale;
+        }
 
-		// Setup the first shower
-		currentShower = 0;
-		startDelay = showerConfigs[currentShower].startDelay;
-	}
+        // If there's another shower, start the timer for it
+        if (showerConfigs.Length > currentShower + 1) {
+            currentShower++;
+            startDelay = showerConfigs[currentShower].startDelay;
+        }
+    }
 
-	/**
-	 * Stop any further showers from occurring.
-	 */
-	public void Suspend() {
-		startDelay = 0;
-	}
+    /**
+     * Setup the shower configs.
+     */
+    public void Setup(FireShower[] showers) {
+        showerConfigs = showers;
+
+        // Setup the first shower
+        currentShower = 0;
+        startDelay = showerConfigs[currentShower].startDelay;
+    }
+
+    /**
+     * Stop any further showers from occurring.
+     */
+    public void Suspend() {
+        startDelay = 0;
+    }
 }
