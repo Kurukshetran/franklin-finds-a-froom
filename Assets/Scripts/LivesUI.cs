2d8b8f9f8e609d00e143ccb59ef307373126f4ab
Jon Uy <jonxuy@gmail.com>
2014-07-02 21:07:22 -0400

    Converting indents to spaces in LivesUI.cs

--- Assets/Scripts/LivesUI.cs
+++ Assets/Scripts/LivesUI.cs
@@ -1,52 +1,52 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class LivesUI : MonoBehaviour {
 
-	// Prefab of the sprite object for the life icon
-	public GameObject iconPrefab;
+    // Prefab of the sprite object for the life icon
+    public GameObject iconPrefab;
 
-	// X distance between icons
-	public float iconXPadding;
+    // X distance between icons
+    public float iconXPadding;
 
-	// Array of references to the instantiated icon objects
-	private GameObject[] iconRefs;
+    // Array of references to the instantiated icon objects
+    private GameObject[] iconRefs;
 
     // Text displaying how many coins are collected and how many are needed until a new life
     public GUIText coinCounterText;
 
-	/**
-	 * Update the # of icons shown on the screen
-	 */
-	public void UpdateRemainingLives(int lives) {
-		// Early out if all remaining lives are lost
-		if (lives < 0) {
-			return;
-		}
+    /**
+     * Update the # of icons shown on the screen
+     */
+    public void UpdateRemainingLives(int lives) {
+        // Early out if all remaining lives are lost
+        if (lives < 0) {
+            return;
+        }
 
-		// Clear and rebuild the reference array
-		if (iconRefs != null) {
-			for (int i = iconRefs.Length - 1; i >= 0; i--) {
-				Destroy(iconRefs[i]);
-			}
-		}
-		iconRefs = new GameObject[lives];
+        // Clear and rebuild the reference array
+        if (iconRefs != null) {
+            for (int i = iconRefs.Length - 1; i >= 0; i--) {
+                Destroy(iconRefs[i]);
+            }
+        }
+        iconRefs = new GameObject[lives];
 
-		// Then recreate the icons
-		for (int i = 0; i < lives; i++) {
-			GameObject newIcon = (GameObject)Instantiate(iconPrefab);
+        // Then recreate the icons
+        for (int i = 0; i < lives; i++) {
+            GameObject newIcon = (GameObject)Instantiate(iconPrefab);
 
-			// Set its position
-			if (newIcon.guiTexture) {
-				Rect currInset = newIcon.guiTexture.pixelInset;
-				int insetX = (int)(currInset.x + ((currInset.x + iconXPadding) * i));
-				newIcon.guiTexture.pixelInset = new Rect(insetX, currInset.y, currInset.width, currInset.height);
-			}
+            // Set its position
+            if (newIcon.guiTexture) {
+                Rect currInset = newIcon.guiTexture.pixelInset;
+                int insetX = (int)(currInset.x + ((currInset.x + iconXPadding) * i));
+                newIcon.guiTexture.pixelInset = new Rect(insetX, currInset.y, currInset.width, currInset.height);
+            }
 
-			// Add into the reference array
-			iconRefs[i] = newIcon;
-		}
-	}
+            // Add into the reference array
+            iconRefs[i] = newIcon;
+        }
+    }
 
     /**
      * Update the coin counter.
